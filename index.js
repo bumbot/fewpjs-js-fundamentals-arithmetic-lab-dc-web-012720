8607496edaf53a816344ff7cddd0ac8ba7884eaa
bumbot <pkim1295@gmail.com>
2020-03-09 15:00:00 -0400

    Done.

--- index.js
+++ index.js
@@ -1,2 +1,12 @@
-
 // Write your code here
+const num1 = 31;
+const num2 = 2;
+
+
+let multiply = num1 * num2;
+
+let random = Math.floor(Math.random()) + 1
+
+let mod = 16 % 6;
+
+let max = Math.max(20, 15, 3);
